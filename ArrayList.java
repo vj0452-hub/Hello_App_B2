457cfb147d11c854d640c6ef9055e87c14c49132
unknown <vj0452@gmail.com>
2026-03-25 08:58:29 +0530

    first commit

---
+++ ArrayList.java
@@ -0,0 +1,10 @@
+class ArrayList{
+  public static void main(String[] args ){
+    
+	// This is "Hello App Program"
+	
+    String msg = "This is Dev Branch";
+    System.out.println(msg);
+	
+  }
+}
